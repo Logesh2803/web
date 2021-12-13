68874a70c080b2c226930d2c684dfaccf180a361
Logesh2803 <logeshlogesh331@gmail.com>
2021-12-13 17:25:38 +0530

    Added

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from './Screens/SplashScreen';
+import SettingsScreen from './Screens/SettingsScreen';
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen options={{headerShown: false}} name="Settings" component={SettingsScreen} />
+      <Stack.Screen options={{headerShown: false}} name="Splash" component={SplashScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ Screens/SplashScreen.js
@@ -0,0 +1,126 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { WebView } from 'react-native-webview';
+
+
+var delayInMilliseconds = 5000; //1 second
+
+
+
+const SplashScreen = ( { route, navigation } ) => {
+    const { addressdata } = route.params;
+   
+    const Error = ({ reload }) => {
+        return (
+          <View style={styles.loadingWrapper}>
+               <Image style={styles.imageSize2} source={require('../Images/network-error.png')}   />
+               <Text style={styles.network}>Network Error</Text>
+              <View style={{ flexDirection:"row"}}>
+                    <View style={styles.buttonStyle}>
+                                <Button
+                            onPress={() => { WebViewRef && WebViewRef.reload(); }}
+                            title="reload"
+                            color="#841584"
+                            />
+                    </View>
+                    <View style={styles.buttonStyle}>
+                            <Button
+                        onPress={() => { navigation.replace('Settings') }}
+                        title="Exit"
+                        color="#841584"
+                        />
+                    </View>
+                </View>
+            
+          </View>
+        );
+      };
+
+    function LoadingIndicatorView() {
+        return  (
+            <View style={styles.centerPage}>
+                <Image style={styles.imageSize} source={require('../Images/logo.png')}   />
+            </View>
+
+        )
+      }
+      const reload = () => webview.current.reload();
+    
+      let WebViewRef;
+
+      let jsCode = `
+        var cookie={};
+        document.cookie.split('; ').forEach(function(i){cookie[i.split('=')[0]]=i.split('=')[1]});
+        document.querySelector('#email').value=cookie['email'] || '';
+        document.querySelector('#password').value=cookie['password'] || '';
+        document.querySelector('#login button').onclick = function(){
+            document.cookie = 'email='+document.querySelector('#email').value;
+            document.cookie = 'password='+document.querySelector('#password').value;
+        };
+    `;
+
+
+    return (
+       
+        <WebView
+        ref={WEBVIEW_REF => (WebViewRef = WEBVIEW_REF)}
+        //Loading URL
+        source={{uri: addressdata}}
+        //Enable Javascript support
+        javaScriptEnabled={true}
+        //For the Cache
+        domStorageEnabled={true}
+        //View to show while loading the webpage
+        renderLoading={LoadingIndicatorView}
+        //Want to show the view or not
+        startInLoadingState={true}
+        sharedCookiesEnabled={true}
+        injectedJavaScript={jsCode}
+        renderError={() => <Error reload={reload} />}
+      />
+
+        
+    )
+}
+
+export default SplashScreen
+
+const styles = StyleSheet.create({
+    centerPage: {
+            alignItems: 'center'
+      },
+      imageSize:{
+
+          width: '70%',
+          height: '70%'
+      },
+      imageSize2:{
+
+        width: 180,
+        height: 180,
+    },
+    network:{
+        marginHorizontal: 10,
+        marginTop: 50,
+        color: 'white',
+        fontSize: 20
+    },
+      loadingWrapper: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black',
+        alignItems: 'center',
+        height: '100%',
+        width:'100%'
+      },
+      buttonStyle: {
+        marginHorizontal: 5,
+        marginTop: 5,
+        width: '30%',
+        height: '100%',
+        paddingHorizontal: 20,
+        paddingVertical: 20,
+      }
+})
